a2010535f03a4386e04253777a2c76c26f2c473c
axytho <jojojo.jonas888@hotmail.com>
2020-03-11 15:18:38 +0100

    Making cordic work (too slow though) and commentary

--- gmsk100-2020-model/model/iq_downmixer.m
+++ gmsk100-2020-model/model/iq_downmixer.m
@@ -5,19 +5,51 @@ function complex_envelope = iq_downmixer(signal, osr, br, fc, fs)
 for k=1:nbcols
     % IQ downmixer
     t = ((1 : numel(signal(:,k)))' - 1) / fs;
-    [cos, sin] = cordic(2 * pi * fc *t,15)
-    upsampled_envelope_Q = cos .* signal(:,k);
-    upsampled_envelope_I = sin .* signal(:,k);
+    cordic = 0;
+    if cordic
+        cosCordic = zeros(length(t), 1);
+        sinCordic = zeros(length(t), 1);
+        for i =1:length(t)
+            v = cordic(2 * pi * fc *t(i),6);
+            cosCordic(i) = v(1);
+            sinCordic(i) = v(2);
+        end
+    else
+        %upsampled_envelope = 2 * exp(-1j * 2 * pi * fc * t) .* signal;
+        cosCordic = cos(2*pi*fc*t);
+        sinCordic = sin(2*pi*fc*t);
+    end
+    upsampled_envelope_I = cosCordic .* signal(:,k);
+    upsampled_envelope_Q = sinCordic .* signal(:,k);
 
     % apply a simple downsampling filter
-    filt = ones(2 * round(fs / (br * osr)) + 1)     ;
-    upsampled_envelope = conv(upsampled_envelope, filt / sum(filt), 'same');
+%     filt = ones(2 * round(fs / (br * osr)) + 1)     ;
+
+
+    % This is simply a crude block filter, use a matched filter instea,
+    % this means instead of our impulse response being a series of ones our
+    % impulse response should be the pulse shape.
+    %plot shape:
+    %plot(upsampled_envelope_Q)
+    
+    
+    filt = ones(2 * round(fs / (br *     osr)) + 1); 
+    % elements in a pulse = time in a pulse / time in a pulse per element
+    % = 1/br / 
+    upsampled_envelope_I = conv(upsampled_envelope_Q, filt / sum(filt), 'same');
+    upsampled_envelope_Q = conv(upsampled_envelope_I, filt/sum(filt), 'same');
+    
+    upsampled_envelope = upsampled_envelope_I + upsampled_envelope_Q * 1i;
 
     % calculate number of output samples
     n1 = numel(upsampled_envelope);
     n2 = round((n1 - 1) * (br * osr) / fs) + 1;
 
     % resample the complex envelope to the new sample rate
+    % (This is a part you don't see in couch because we're working in the
+    % continous domain, but in the discrete domain, since we've just done a
+    % low pass filter, we can throw away everything on the left and right
+    % side of the filter)
     t1 = ((1 : n1)' - 1) / fs;
     t2 = ((1 : n2)' - 1) / (br * osr);
     complex_envelope(:,k) = interp1(t1, upsampled_envelope, t2);
